8a0ee9feb7c03ccf56c07d04de10fe7fec91b9c3
Milanh3h3 <149052315+Milanh3h3@users.noreply.github.com>
2024-05-24 11:47:45 +0200

    LoginPagina Frontend af

--- frontend-b2b/src/Components/Login.js
+++ frontend-b2b/src/Components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import './Login.css'; 
+import { FaUser, FaEnvelope, FaLock } from 'react-icons/fa'; // Import icons
+import './Login.css';
 
 const LoginForm = () => {
   const [PersoneelsNummer, setPersoneelsNummer] = useState('');
@@ -13,43 +14,55 @@ const LoginForm = () => {
 
   return (
     <div className="container">
-      <form className="login-form" onSubmit={handleSubmit}>
-        <div className="login-header">
-          <h1>Inloggen</h1>
-        </div>
-        <hr className="DividerLine" />
-        <div className="form-group">
-          <input
-            type="text"
-            id="PersoneelsNummer"
-            placeholder="Personeelsnummer"
-            value={PersoneelsNummer}
-            onChange={(event) => setPersoneelsNummer(event.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            id="Email"
-            placeholder="Je login of e-mailadres"
-            value={Email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            id="Wachtwoord"
-            placeholder="Wachtwoord"
-            value={Wachtwoord}
-            onChange={(event) => setWachtwoord(event.target.value)}
-          />
-        </div>
-            <div className="form-actions">
-            <a href="#">Wachtwoord vergeten?</a>
+      <div className="form-container">
+        <form className="login-form" onSubmit={handleSubmit}>
+          <div className="login-header">
+            <h1>Inloggen</h1>
+          </div>
+          <hr className="DividerLine1" />
+          <div className="form-group">
+            <FaUser className="icon" />
+            <input
+              type="text"
+              id="PersoneelsNummer"
+              placeholder="Je personeelsnummer"
+              value={PersoneelsNummer}
+              onChange={(event) => setPersoneelsNummer(event.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <FaEnvelope className="icon" />
+            <input
+              type="email"
+              id="Email"
+              placeholder="Je login of e-mailadres"
+              value={Email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <FaLock className="icon" />
+            <input
+              type="password"
+              id="Wachtwoord"
+              placeholder="Wachtwoord"
+              value={Wachtwoord}
+              onChange={(event) => setWachtwoord(event.target.value)}
+            />
+          </div>
+          <div className="form-actions">
+            <a href="http://localhost:3000/">Wachtwoord vergeten?</a>
             <button type="submit">Log in</button>
-        </div>
-      </form>
+          </div>
+          <div className="footer">
+            <p>Mede mogelijk gemaakt door</p>
+            <hr className="DividerLine2" />
+            <img src="/UMCUtrechtLogo.png" alt="UMC Utrecht" />
+            <img src="/JDB logo.png" alt="JDB" />
+          </div>
+        </form>
+      </div>
+      <img className="image-container"src="/Kinderen.png" alt="Kinderen" height={700}/>
     </div>
   );
 };
