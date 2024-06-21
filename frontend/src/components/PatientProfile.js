fee43bf25e5e6250326f238fd766d60d0dc2f13a
finn4kill <finn.curfs@gmail.com>
2024-06-21 10:58:59 +0200

    Patient verslagen hardcoded frontend /patientprofiel met routing op een basic pagina

---
+++ frontend/src/components/PatientProfile.js
@@ -0,0 +1,24 @@
+import React, {  } from 'react';
+import './PatientProfile.css';
+
+const PatientProfile = () => {
+    return(
+        <>
+        <head>
+            <title>Patientprofiel</title>
+        </head>
+        <body>
+            <h1>Patient profiel</h1>
+            <h3>Kies een verslag</h3>
+        </body>
+        <div>
+        <p>Maandag 15-06</p>
+        <p>Woensdag 09-04</p>
+        <p>Woensdag 09-04</p>
+
+        <button>Toevoegen</button>
+        </div>
+        </>
+    )
+}
+export default PatientProfile
