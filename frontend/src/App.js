c1811c44f5cb12916ce44be780b3c9f871b14eff
WouterVinclair <woutervinclair@gmail.com>
2024-06-21 13:02:44 +0200

    Merge branch 'main' into feature/sidepanel-default

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,34 @@
-// src/App.js
-
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Loginpage from './components/Login';
+import Dashboard from './components/Dashboard';
+import PatientList from './components/PatientList';
+import ResultDetails from './components/ResultDetails';
+import Patientprofile from './components/Patientprofile';
 
 function App() {
     return (
-        <div className="App">
-          <Loginpage />
-        </div>
-      );
+        <Router>
+            <div className="App">
+                <Routes>
+                    {/* Route voor de inlogpagina */}
+                    <Route path="/" element={<Loginpage />} />
+
+                    {/* Route voor het dashboard */}
+                    <Route path="/dashboard" element={<Dashboard />} />
+
+                    {/* Route voor de lijst met pati�nten */}
+                    <Route path="/patientList" element={<PatientList />} />
+
+                    {/* Route voor individuele pati�ntendetails */}
+                    <Route path="/patient-profile/:id" element={<Patientprofile />} />
+
+                    {/* Route voor resultaatdetails */}
+                    <Route path="/resultDetails" element={<ResultDetails />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/Patientprofile.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Patientprofile.css';
+
+const PatientProfile = () => {
+    // Dummy patiëntgegevens
+    const patient = {
+        name: "Joep Doe",
+        phone: "+3264981949",
+        email: "joepdoetmail@gmail.com",
+        diagnosis: "Juveniele Dermatomyosites",
+        profilePictureUrl: "profile-picture-url.jpg"
+    };
+
+    return (
+        <div className="patient-profile">
+            <div className="header">
+                <div className="logo">JDB</div>
+                <div className="title">Profiel van {patient.name}</div>
+            </div>
+            <div className="profile-picture">
+                <img src={patient.profilePictureUrl} alt="Profile" />
+                <div className="patient-name">{patient.name}</div>
+            </div>
+            <div className="patient-details">
+                <div><strong>Telefoon:</strong> {patient.phone}</div>
+                <div><strong>Email:</strong> {patient.email}</div>
+                <div><strong>Diagnose:</strong> {patient.diagnosis}</div>
+            </div>
+            <button className="patient-data-button">Patientgegevens</button>
+        </div>
+    );
+}
+
+export default PatientProfile;
