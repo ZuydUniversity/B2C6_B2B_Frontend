aaeb134e369f40f91c29832800fda1e0538281ca
finn4kill <finn.curfs@gmail.com>
2024-06-25 11:53:25 +0200

    Patient profiel verslagen frontend hardcoded

--- frontend/src/components/Patientprofile.js
+++ frontend/src/components/Patientprofile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Patientprofile.css';
 
 const PatientProfile = () => {
-    // Dummy patiëntgegevens
+    // Dummy patientgegevens
     const patient = {
         name: "Joep Doe",
         phone: "+3264981949",
@@ -12,22 +12,37 @@ const PatientProfile = () => {
     };
 
     return (
-        <div className="patient-profile">
-            <div className="header">
-                <div className="logo">JDB</div>
-                <div className="title">Profiel van {patient.name}</div>
-            </div>
-            <div className="profile-picture">
-                <img src={patient.profilePictureUrl} alt="Profile" />
-                <div className="patient-name">{patient.name}</div>
-            </div>
-            <div className="patient-details">
-                <div><strong>Telefoon:</strong> {patient.phone}</div>
-                <div><strong>Email:</strong> {patient.email}</div>
-                <div><strong>Diagnose:</strong> {patient.diagnosis}</div>
-            </div>
-            <button className="patient-data-button">Patientgegevens</button>
+    <div className="patient-profile">
+        <div className="header">
+            <div className="logo">JDB</div>
+            <div className="title">Profiel van {patient.name}</div>
         </div>
+        <div className="profile-picture">
+            <img src={patient.profilePictureUrl} alt="Profile" />
+            <div className="patient-name">{patient.name}</div>
+        </div>
+        <div className="patient-details">
+            <div><strong>Telefoon:</strong> {patient.phone}</div>
+            <div><strong>Email:</strong> {patient.email}</div>
+            <div><strong>Diagnose:</strong> {patient.diagnosis}</div>
+        </div>
+        <button className="patient-data-button">Patientgegevens</button>
+
+        <div className="patient-reports">
+            <div className="reports-header">
+                <div className="report-title">Kies je verslag</div>
+                <button className="add-report-button">Voeg verslag toe</button>
+            </div>
+            <div className="report-list">
+                <div className="report-item">
+                    <p>Verslag 6/25/2024</p>
+                </div>
+            </div>
+            <div className="report-search">
+                <input type="text" placeholder="Afspraak zoeken" />
+            </div>
+        </div>
+    </div>
     );
 }
 
